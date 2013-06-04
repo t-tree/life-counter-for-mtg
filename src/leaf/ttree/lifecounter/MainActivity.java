0120cd90292bf68e26819e9b43510bbe80a11a7e
t-tree <ttree.leaf@gmail.com>
2013-06-05 00:01:06 +0900

    add chronometer start method #1 fixed

--- src/leaf/ttree/lifecounter/MainActivity.java
+++ src/leaf/ttree/lifecounter/MainActivity.java
@@ -3,13 +3,19 @@ package leaf.ttree.lifecounter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Chronometer;
 
 public class MainActivity extends Activity {
+
+	Chronometer chrono;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		chrono = (Chronometer) this.findViewById(R.id.chronometer);
 	}
 
 	@Override
@@ -17,6 +23,10 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	public void clickStartPauseButton(View v) {
+		chrono.start();
 	}
 
 }
